bab92f5f916e3302278cb2177a831e4166017f47
h-kumar <k.harish@outlook.com>
2021-11-11 22:15:51 +0530

    added vsearch funtiona as a distributed module.

---
+++ Chapter_4/mymodules/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name ='vsearch',
+    version='1.0',
+    description='Vowel search in a phrase',
+    author='hkumar',
+    author_email='',
+    url='',
+    py_modules=['vsearch']
+
+
+
+
+)
