b1a0158ce18e9d03ac11149f643c6251ab5baadb
h-kumar <k.harish@outlook.com>
2021-11-11 23:17:51 +0530

    formatted coded to pep-8 guidelines

--- Chapter_4/mymodules/vsearch.py
+++ Chapter_4/mymodules/vsearch.py
@@ -1,12 +1,9 @@
-def search_for_vowels(phrase:str) -> set:
+def search_for_vowels(phrase: str) -> set:
     """Return any vowels found in a word or sentence."""
     vowel = set('aeiou')
     return vowel.intersection(set(phrase))
 
-# search_for_vowels(word)
-# word = input("Please input a word or sentence: ")
 
-def search_for_letters(phrase:str, letters:str ='aeiou') -> set:
+def search_for_letters(phrase: str, letters: str = 'aeiou') -> set:
     """Return a set of letters found in phrase"""
     return set(letters).intersection(set(phrase))
-
