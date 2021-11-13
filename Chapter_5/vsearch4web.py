34280f5c0778c149fa01dd22814d29d3c68bfe80
h-kumar <k.harish@outlook.com>
2021-11-13 19:59:24 +0530

    removed the redirect module and changed the code to redirect from / to /entry.

--- Chapter_5/vsearch4web.py
+++ Chapter_5/vsearch4web.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request#, redirect
 from werkzeug.utils import HTMLBuilder
 from vsearch import search_for_letters
 
+"""Identify the currently active namespace using __name__"""
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello() -> '302':
-    return redirect('/entry')
-
-
+"""Use the @ before a function to identify it as a decorator. This lets you change the funtion's behavior
+without changing the funtion's code"""
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+    """This function invokes the search_for_letters module and maps the html field to the search_for_letters
+    method as arguments, and renders the result on another html page."""
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your Results:'
@@ -23,10 +22,12 @@ def do_search() -> 'html':
                            the_results=results)
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='Welcome to search4letters on the Web!')
 
-
-app.run(debug=True)
+"""When deploying to a server, wrap the app.run(debug=True) in a dunder name == dunder main if statement."""
+if __name__ == '__main__':
+    app.run(debug=True)
